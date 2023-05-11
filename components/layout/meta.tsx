f56b7d05bfc0bf80b8ca30756f3e6a62026a5f33
Coformatique <coformatique@Coformatiques-MacBook-Pro.local>
2023-05-11 12:48:24 +0200

    Clean og

--- components/layout/meta.tsx
+++ components/layout/meta.tsx
@@ -5,7 +5,7 @@ const DOMAIN = "https://precedent.dev";
 export default function Meta({
   title = "Carry it, let'em track",
   description = "Track your orders in real-time, every step of the way",
-  image = `${DOMAIN}/api/og`,
+  // image = `${DOMAIN}/api/og`,
 }: {
   title?: string;
   description?: string;
@@ -19,18 +19,18 @@ export default function Meta({
 
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <meta itemProp="image" content={image} />
+      {/* <meta itemProp="image" content={image} /> */}
       <meta property="og:logo" content={`${DOMAIN}/logo.png`}></meta>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
+      {/* <meta property="og:image" content={image} /> */}
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@abd_elrahmanmo" />
       <meta name="twitter:creator" content="@abd_elrahmanmo" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      {/* <meta name="twitter:image" content={image} /> */}
     </Head>
   );
 }
