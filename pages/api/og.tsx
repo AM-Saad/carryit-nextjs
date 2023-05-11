26baf26121bfd7bafa6c44d86641941f373f4de9
Coformatique <coformatique@Coformatiques-MacBook-Pro.local>
2023-05-11 12:45:59 +0200

    Try deployment

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,66 +1,66 @@
-/* eslint-disable @next/next/no-img-element */
-import { ImageResponse } from "@vercel/og";
-import { NextRequest } from "next/server";
-import Image from "next/image";
-export const config = {
-  runtime: "experimental-edge",
-  api:{
-    bodyParser: true,
-  }
-};
+// /* eslint-disable @next/next/no-img-element */
+// import { ImageResponse } from "@vercel/og";
+// import { NextRequest } from "next/server";
+// import Image from "next/image";
+// export const config = {
+//   runtime: "experimental-edge",
+//   api:{
+//     bodyParser: true,
+//   }
+// };
 
-const sfPro = fetch(
-  new URL("../../styles/SF-Pro-Display-Medium.otf", import.meta.url),
-).then((res) => res.arrayBuffer());
+// const sfPro = fetch(
+//   new URL("../../styles/SF-Pro-Display-Medium.otf", import.meta.url),
+// ).then((res) => res.arrayBuffer());
 
-export default async function handler(req: NextRequest) {
-  const [sfProData] = await Promise.all([sfPro]);
+// export default async function handler(req: NextRequest) {
+//   const [sfProData] = await Promise.all([sfPro]);
 
-  const { searchParams } = req.nextUrl;
-  const title = searchParams.get("title") || "Precedent";
+//   const { searchParams } = req.nextUrl;
+//   const title = searchParams.get("title") || "Precedent";
 
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          height: "100%",
-          width: "100%",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          backgroundColor: "white",
-          backgroundImage:
-            "linear-gradient(to bottom right, #E0E7FF 25%, #ffffff 50%, #CFFAFE 75%)",
-        }}
-      >
-        <Image src={new URL("../../public/logo.png", import.meta.url).toString()} alt="Precedent Logo" width={80} height={80} />
+//   return new ImageResponse(
+//     (
+//       <div
+//         style={{
+//           height: "100%",
+//           width: "100%",
+//           display: "flex",
+//           flexDirection: "column",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           backgroundColor: "white",
+//           backgroundImage:
+//             "linear-gradient(to bottom right, #E0E7FF 25%, #ffffff 50%, #CFFAFE 75%)",
+//         }}
+//       >
+//         <Image src={new URL("../../public/logo.png", import.meta.url).toString()} alt="Precedent Logo" width={80} height={80} />
    
-        <h1
-          style={{
-            fontSize: "100px",
-            fontFamily: "SF Pro",
-            background:
-              "linear-gradient(to bottom right, #000000 21.66%, #78716c 86.47%)",
-            backgroundClip: "text",
-            color: "transparent",
-            lineHeight: "5rem",
-            letterSpacing: "-0.02em",
-          }}
-        >
-          {title}
-        </h1>
-      </div>
-    ),
-    {
-      width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: "SF Pro",
-          data: sfProData,
-        },
-      ],
-    },
-  );
-}
+//         <h1
+//           style={{
+//             fontSize: "100px",
+//             fontFamily: "SF Pro",
+//             background:
+//               "linear-gradient(to bottom right, #000000 21.66%, #78716c 86.47%)",
+//             backgroundClip: "text",
+//             color: "transparent",
+//             lineHeight: "5rem",
+//             letterSpacing: "-0.02em",
+//           }}
+//         >
+//           {title}
+//         </h1>
+//       </div>
+//     ),
+//     {
+//       width: 1200,
+//       height: 630,
+//       fonts: [
+//         {
+//           name: "SF Pro",
+//           data: sfProData,
+//         },
+//       ],
+//     },
+//   );
+// }
